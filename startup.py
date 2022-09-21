fe9620516573861ecedbc7ab9862a2d1859f148d
Geronaso <cezarmgt@gmail.com>
2022-09-21 20:32:58 -0300

    Azure Integration

--- startup.py
+++ startup.py
@@ -1,12 +1,5 @@
-""" 
-In this sample, the Flask app object is contained within the hello_app *module*;
-that is, hello_app contains an __init__.py along with relative imports. Because
-of this structure, a file like webapp.py cannot be run directly as the startup
-file through Gunicorn; the result is "Attempted relative import in non-package".
-
-The solution is to provide a simple alternate startup file, like this present
-startup.py, that just imports the app object. You can then just specify
-startup:app in the Gunicorn command.
+"""
+Solution to give gunicorn a path to create_app method in Azure
 """
 
 from flask_sort import create_app
